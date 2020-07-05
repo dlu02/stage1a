075a5a7ac4b8a00b40d21af951bc25f443f2b645
Damien Lu <ludamien2@gmail.com>
2020-07-05 22:13:38 +0200

    correction loi expo convo

--- lois/expo_convo.php
+++ lois/expo_convo.php
@@ -34,7 +34,7 @@
 		<h2 class="titre">Étude de lois</h2>
 		<h3>Loi exponentielle convolution <a href=lois.php class=button_link>Choisir une autre loi</a></h3>
 		Par l'écriture de la densité de la loi exponentielle convolution, pour $x>0$ et $a,b > 0$ :
-		$$f(x,a,b)=\dfrac{ab}{b-a}(\mathrm{e}^{-ax}-\mathrm{e}^{-bx})$$
+		$$f(x,a,b)=\dfrac{ab}{b-a}(\mathrm{e}^{-ax}-\mathrm{e}^{-bx})=f(x,a,b)= \dfrac{b}{b-a}a\mathrm{e}^{-ax}-\dfrac{a}{b-a}b\mathrm{e}^{-bx}$$
 		nous constatons que
 		<p>- dans le cas où $a\neq b$, il s'agit d'un mélange de deux variables iid exponentielles de paramètres différents a et b qui sont donc indépendantes (nous avons en fait fait l'opération de convolution de deux variables)</p>
 		<p>- dans le cas où $a=b$, nous retombons sur la somme de deux variables iid exponentielles de même paramètre $a=b$, et on montre que cette somme suit une loi de Gamma de paramètres 2 et $a=b$.</p>
@@ -42,13 +42,13 @@
 
 		<tr> <th colspan="2">Propriétés de la loi exponentielle convolution (cas où $a\neq b$)</th> </tr>
 
-		<tr><td>Densité</td><td>$ \displaystyle  f(x,a,b)= \dfrac{ab}{b-a}(\mathrm{e}^{-ax}-\mathrm{e}^{-bx}) \mathbb{1}_{]0,+\infty[}(x)$</td></tr>
+		<tr><td>Densité</td><td>$ \displaystyle  f(x,a,b)= \dfrac{b}{b-a}a\mathrm{e}^{-ax}-\dfrac{a}{b-a}b\mathrm{e}^{-bx} \mathbb{1}_{]0,+\infty[}(x)$</td></tr>
 		<tr><td>Domaines des paramètres</td><td>$a>0,b>0,a\neq b$</td></tr>
 		<tr><td>Fonction de répartition</td>
 		<td>Pour $x>0$, $\displaystyle F(x,a,b)=1+\dfrac{a}{b-a}\mathrm{e}^{-b x} - \dfrac{b}{b-a} \mathrm{e}^{-a x}$</td></tr>
-		<tr><td>Moments d'ordre $k$</td><td>$\displaystyle \mathbb{E}[X^k]=k! \sum_{i=0}^k \dfrac{1}{a^i} \dfrac{1}{b^{k-i}}$</td></tr>
+		<tr><td>Moments d'ordre $k$</td><td>$\displaystyle \mathbb{E}[X^k]=\dfrac{b}{b-a} \dfrac{k!}{a^k} - \dfrac{a}{b-a} \dfrac{k!}{b^k}$</td></tr>
 		<tr><td>$\displaystyle E(X)$</td><td>$\dfrac{1}{a}+\dfrac{1}{b}$</td></tr>
-		<tr><td>$\displaystyle V(X)$</td><td>$2\left(\dfrac{1}{a^2}+\dfrac{1}{ab}+\dfrac{1}{b^2}\right) - \dfrac{1}{a} - \dfrac{1}{b}$</td></tr>
+		<tr><td>$\displaystyle V(X)$</td><td>$\dfrac{1}{a^2}+\dfrac{1}{b^2}$</td></tr>
 		<tr><td>Skewness</td><td>$\displaystyle \gamma_{1}=\dfrac{2\left(\dfrac{1}{a^3}+\dfrac{1}{b^3}\right)}{\left(\dfrac{1}{a^2}+\dfrac{1}{b^2}\right)^{3/2}}$</td></tr>
 		<tr><td>Kurtosis</td><td>$\displaystyle \gamma_{2}=\dfrac{6\left( \dfrac{1}{a^4}+\dfrac{1}{b^4} \right)}{\left(\dfrac{1}{a^2}+\dfrac{1}{b^2} \right)^2}$</td></tr>
 
