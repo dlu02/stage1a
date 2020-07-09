8602739b380addd51f8b57509b4abf848d50d45d
Damien Lu <ludamien2@gmail.com>
2020-07-09 16:45:51 +0200

    correction bug histogramme

--- lois.py
+++ lois.py
@@ -101,26 +101,23 @@ def histo_discretes(data, nom=None):  # Histogramme des données discrètes
         plt.close()
 
 
-def histo_Continue(data, k, nom=None):  # histogramme pour les Données Continues
+def histo_Continue(data, k, nom=None):
     # k=nombre de classes
     # nom : le nom (sans extension)  pour la figure
-
-    plt.rcParams['hatch.color'] = [0.9, 0.9, 0.9]  # hachure
-
+    # hachure
+    plt.rcParams['hatch.color'] = [0.9, 0.9, 0.9]
     data = np.array([x for x in data])
     Ext = [min(data)+(max(data)-min(data))*i/(1.0*k) for i in range(k+1)]
-
-    C = [0.5*(Ext[i]+Ext[i+1]) for i in range(k)]  # Centres des classes
-
-    NN = []  # Effectifs des classes
+    # Centres des classes
+    C = [0.5*(Ext[i] + Ext[i+1]) for i in range(k)]
+    # Effectifs des classes
+    NN = []
     for i in range(k):
         NN.append(((Ext[i] <= data) & (data <= Ext[i+1])).sum())
-
     # pour la classe modale
     indice_max = [i for i in range(k) if NN[i] == np.max(NN)]
-
     TT = [str("{:.4f}".format(t)) for t in Ext]  # pour afficher les extrémités des classes
-
+    # CC = [str("{:.4f}".format(t)) for t in C]   # pour afficher les centres des classes
     fig = plt.figure(figsize=(10, 7))
     ax1 = fig.add_subplot(111)
     # cacher le cadre
@@ -130,28 +127,26 @@ def histo_Continue(data, k, nom=None):  # histogramme pour les Données Continue
     ax1.xaxis.set_ticks_position('bottom')
 
     ax1.set_yticks([])
-    largeur = Ext[1]-Ext[0]   # largeur des classes
+    largeur = Ext[1] - Ext[0]  # largeur des classes
 
     for i in range(k):
         if i in indice_max:
-            ax1.bar(C[i], NN[i], largeur,  color=[0.15, 0.15, 0.80], edgecolor="white", hatch="/", lw=1., zorder=0, alpha=0.9)
+            ax1.bar(C[i], NN[i], largeur, color=[0.15, 0.15, 0.80], edgecolor="white", hatch="/", lw=1., zorder=0, alpha=0.9)
         else:
             ax1.bar(C[i], NN[i], largeur, align='center', edgecolor="white")
-    ax1.text(C[i], NN[i], "%d" % (NN[i]), fontsize=8, style='italic', horizontalalignment='center', verticalalignment='bottom')
-
+        ax1.text(C[i], NN[i], "%d" % (NN[i]), fontsize=8, style='italic', horizontalalignment='center', verticalalignment='bottom')
     ax1.set_xticks(Ext)  # positions des extrémités des classes
-
     ax1.set_xticklabels(TT, fontsize=8, rotation=45)
-
+    # ax1.set_xticklabels(CC ,fontsize=8, rotation=45)
     ax1.set_xlim(np.min(data)-0.75*largeur, np.max(data)+0.75*largeur)
-    ax1.set_ylim(0.0, np.max(NN)+3.0)
+    ax1.set_ylim(0.0, np.max(NN) + 3.0)
     ax1.set_xlabel("Valeurs", fontsize=12)
     ax1.set_ylabel("Effectifs", fontsize=14)
 
     if nom is None:
         plt.show()
     else:
-        plt.savefig("hist.png")
+        plt.savefig("hist.png", dpi=400)
         plt.close()
 
 
--- stats.py
+++ stats.py
@@ -91,26 +91,23 @@ def histo_discretes(data, nom=None):  # Histogramme des données discrètes
         plt.close()
 
 
-def histo_Continue(data, k, nom=None):  # histogramme pour les Données Continues
+def histo_Continue(data, k, nom=None):
     # k=nombre de classes
     # nom : le nom (sans extension)  pour la figure
-
-    plt.rcParams['hatch.color'] = [0.9, 0.9, 0.9]  # hachure
-
+    # hachure
+    plt.rcParams['hatch.color'] = [0.9, 0.9, 0.9]
     data = np.array([x for x in data])
     Ext = [min(data)+(max(data)-min(data))*i/(1.0*k) for i in range(k+1)]
-
-    C = [0.5*(Ext[i]+Ext[i+1]) for i in range(k)]  # Centres des classes
-
-    NN = []  # Effectifs des classes
+    # Centres des classes
+    C = [0.5*(Ext[i] + Ext[i+1]) for i in range(k)]
+    # Effectifs des classes
+    NN = []
     for i in range(k):
         NN.append(((Ext[i] <= data) & (data <= Ext[i+1])).sum())
-
     # pour la classe modale
     indice_max = [i for i in range(k) if NN[i] == np.max(NN)]
-
     TT = [str("{:.4f}".format(t)) for t in Ext]  # pour afficher les extrémités des classes
-
+    # CC = [str("{:.4f}".format(t)) for t in C]   # pour afficher les centres des classes
     fig = plt.figure(figsize=(10, 7))
     ax1 = fig.add_subplot(111)
     # cacher le cadre
@@ -120,28 +117,26 @@ def histo_Continue(data, k, nom=None):  # histogramme pour les Données Continue
     ax1.xaxis.set_ticks_position('bottom')
 
     ax1.set_yticks([])
-    largeur = Ext[1]-Ext[0]   # largeur des classes
+    largeur = Ext[1] - Ext[0]  # largeur des classes
 
     for i in range(k):
         if i in indice_max:
-            ax1.bar(C[i], NN[i], largeur,  color=[0.15, 0.15, 0.80], edgecolor="white", hatch="/", lw=1., zorder=0, alpha=0.9)
+            ax1.bar(C[i], NN[i], largeur, color=[0.15, 0.15, 0.80], edgecolor="white", hatch="/", lw=1., zorder=0, alpha=0.9)
         else:
             ax1.bar(C[i], NN[i], largeur, align='center', edgecolor="white")
-    ax1.text(C[i], NN[i], "%d" % (NN[i]), fontsize=8, style='italic', horizontalalignment='center', verticalalignment='bottom')
-
+        ax1.text(C[i], NN[i], "%d" % (NN[i]), fontsize=8, style='italic', horizontalalignment='center', verticalalignment='bottom')
     ax1.set_xticks(Ext)  # positions des extrémités des classes
-
     ax1.set_xticklabels(TT, fontsize=8, rotation=45)
-
+    # ax1.set_xticklabels(CC ,fontsize=8, rotation=45)
     ax1.set_xlim(np.min(data)-0.75*largeur, np.max(data)+0.75*largeur)
-    ax1.set_ylim(0.0, np.max(NN)+3.0)
+    ax1.set_ylim(0.0, np.max(NN) + 3.0)
     ax1.set_xlabel("Valeurs", fontsize=12)
     ax1.set_ylabel("Effectifs", fontsize=14)
 
     if nom is None:
         plt.show()
     else:
-        plt.savefig("hist.png")
+        plt.savefig("hist.png", dpi=400)
         plt.close()
 
 
@@ -171,7 +166,7 @@ def do_operation(array, n):
                                     if (discretes_continues(array) == "discrete"):
                                         histo_discretes(array, nom="hist")
                                     if (discretes_continues(array) == "continue"):
-                                        classe = int(5*math.log10(args.taille))
+                                        classe = int(5*math.log10(args.size2))
                                         histo_Continue(array, classe, nom="hist")
                                 else:
                                     return "Saisie invalide."
@@ -215,8 +210,8 @@ print(res)
 ######
 
 if np.size(m) != args.size2:
-	print("Erreur de saisie : la taille saisie ne correspond pas à la taille des données.")
+    print("Erreur de saisie : la taille saisie ne correspond pas à la taille des données.")
 else:
-	L = {"min": do_operation(m, 0), "max": do_operation(m, 1), "moy": do_operation(m, 2), "var": do_operation(m, 3), "ec": do_operation(m, 4), "skew": do_operation(m, 5), "kurt": do_operation(m, 6), "hist": do_operation(m, 7)}  # dictionnaire des valeurs à calculer
-	res = json.dumps(L, default=convert)  # appliquer convert si le nombre est un entier numpy
-	print(res)
+    L = {"min": do_operation(m, 0), "max": do_operation(m, 1), "moy": do_operation(m, 2), "var": do_operation(m, 3), "ec": do_operation(m, 4), "skew": do_operation(m, 5), "kurt": do_operation(m, 6), "hist": do_operation(m, 7)}  # dictionnaire des valeurs à calculer
+    res = json.dumps(L, default=convert)  # appliquer convert si le nombre est un entier numpy
+    print(res)
