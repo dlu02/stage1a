bad0f468fcba7f511c2607536c814283f00b6e37
Damien Lu <ludamien2@gmail.com>
2020-07-14 18:17:22 +0200

    correction chemins css

--- stats.php
+++ stats.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="/stage/css/style_page.css">
+  <link rel="stylesheet" type="text/css" href="css/style_page.css">
   <title>Résultats de l'analyse</title>
 </head>
 <body>
