1b6b355e34e55513c2aa6309531bba6b6412fde9
Damien Lu <ludamien2@gmail.com>
2020-07-28 12:58:50 +0200

    correction loi hyperexpo

--- lois/hyperexpo.php
+++ lois/hyperexpo.php
@@ -40,7 +40,7 @@
 		<table class="alternate">
 		<tr> <th colspan="4">Propriétés de la loi hyperexponentielle</th> </tr>
 		<tr><td>Densité</td><td>$ \displaystyle  f(x,a,b)= \dfrac{ab}{a+b}\left(\mathrm{e}^{-ax}+\mathrm{e}^{-bx}\right) \mathbb{1}_{]0,+\infty[}(x)$</td><td>$\displaystyle E(X)$</td><td>$\dfrac{b}{a(a+b)}+\dfrac{a}{b(a+b)}$</td></tr>
-		<tr><td>Domaines des paramètres</td><td>$ a >0 \,$  ,  $ b >0 $</td><td>$\displaystyle V(X)$</td><td>$\dfrac{b^2-2a^2+2ab}{a^2(a+b)^2}+\dfrac{a^2+2ab}{b^2(a+b)^2}$</td></tr>
+		<tr><td>Domaines des paramètres</td><td>$ a >0 \,$  ,  $ b >0 $</td><td>$\displaystyle V(X)$</td><td>$\dfrac{b^2+2ab}{a^2(a+b)^2} + \dfrac{a^2 + 2ab}{b^2(a+b)^2}$</td></tr>
 		<tr><td>Fonction de répartition</td>
 		<td>Pour $x>0$, $\displaystyle F(x,a,b)=\dfrac{a b}{a+b}\left(\dfrac{1-\mathrm{e}^{-a x}}{a} + \dfrac{1-\mathrm{e}^{-b x}}{b} \right)$</td><td>Skewness</td><td>$\displaystyle \gamma_{1}=\dfrac{M_3-3E(X)V(X)-E(X)^3}{V(X)^{3/2}}  $</td></tr>
 		<tr><td>Moments d'ordre $k$</td><td>$\displaystyle \mathbb{E}[X^k]=\dfrac{b}{a+b}\dfrac{k!}{a^k}+\dfrac{a}{a+b}\dfrac{k!}{b^k}$</td><td>Kurtosis</td><td>$\displaystyle \gamma_{2}=\dfrac{M_4-4E(X)V(X)^{3/2}\gamma_1-6E(X)^2V(X)-E(X)^4}{V(X)^2} $</td></tr>
